318946d767a291b84c7a763578297ed81132c0f2
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-10-15 14:41:13 +0200

    added middleware for serving static assets

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import bodyparser from 'body-parser';
 import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
+import logRoutes from './routes/log';
+import path from 'path';
 
 dotenv.config();
 
 const app:Express = express();
 app.use(bodyparser.json());
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', logRoutes);
+
+app.use((err: Error, _: Request, res: Response, _2: NextFunction) => {
+    res.status(500).json({ message: err.message });
+});
 
 mongoose.connect(process.env.MONGO_URI ?? '')
     .then(() => console.log('DB Connection successful'))
