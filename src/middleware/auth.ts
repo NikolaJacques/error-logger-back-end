82ccd4c5eb4829e38fa42d633990f23d3dc9a276
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-10-15 22:59:14 +0200

    auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,22 @@
 import {RequestHandler} from 'express';
+import * as jwt from 'jsonwebtoken';
+import { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from '../utils/env';
 
-export const auth:RequestHandler = (_, _2, _3) => {
-    // extract token from headers
-    // decode token
-    // add app id to request body
+export const auth:RequestHandler = (req, res, next) => {
+    try {
+        const token = req.get('Authorization')!.split(' ')[1];
+        const decodedToken = jwt.verify(token, JWT_SECRET ?? '');
+        if (!decodedToken){
+            res.status(401).json({
+                message: 'Could not authenticate; request failed.'
+            });
+        }
+        (req as any).appId = (decodedToken as JwtPayload).appId;
+        (req as any).sessionId = (decodedToken as JwtPayload).sessionId;
+        next();
+    }
+    catch(err){
+        next(err);
+    }
 };
