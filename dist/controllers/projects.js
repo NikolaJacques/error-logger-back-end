bb0a0bf1848e8b685a02f26b00245b3ad640aa4c
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2023-01-18 17:48:04 +0100

    added getProjects controller

--- dist/controllers/projects.js
+++ dist/controllers/projects.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteProject = exports.updateProject = exports.addProject = exports.getProject = void 0;
+exports.deleteProject = exports.updateProject = exports.addProject = exports.getProjects = exports.getProject = void 0;
 const mongoose_1 = require("mongoose");
 const project_1 = __importDefault(require("../models/project"));
 const getProject = async (req, res, _) => {
@@ -20,6 +20,20 @@ const getProject = async (req, res, _) => {
     });
 };
 exports.getProject = getProject;
+const getProjects = async (_, res, _2) => {
+    const projects = await project_1.default.find({});
+    if (projects.length === 0) {
+        const err = new Error();
+        err.message = 'Project query unsuccessful; appId returned no results.';
+        err.statusCode = 404;
+        throw err;
+    }
+    res.status(200).json({
+        message: 'Request successful.',
+        data: projects
+    });
+};
+exports.getProjects = getProjects;
 const addProject = (_, _2, _3) => { };
 exports.addProject = addProject;
 const updateProject = (_, _2, _3) => { };
--- src/controllers/projects.ts
+++ src/controllers/projects.ts
@@ -17,6 +17,20 @@ export const getProject:RequestHandler = async (req, res, _) => {
     });
 };
 
+export const getProjects: RequestHandler = async (_, res, _2) => {
+    const projects = await Project.find({});
+    if (projects.length===0){
+        const err = new Error() as ErrorResponseType;
+        err.message = 'Project query unsuccessful; appId returned no results.'; 
+        err.statusCode = 404;
+        throw err;
+    }
+    res.status(200).json({
+        message: 'Request successful.',
+        data: projects
+    });
+}
+
 export const addProject: RequestHandler = (_, _2, _3) => {};
 export const updateProject: RequestHandler = (_, _2, _3) => {};
 export const deleteProject: RequestHandler = (_, _2, _3) => {};
